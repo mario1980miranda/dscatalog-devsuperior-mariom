df80dc75cd64c97329e14aef82300a8f3d48a1b1
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-09-20 14:35:34 -0400

    Product Insert, Update & Delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,8 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -14,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -25,12 +26,8 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
-	@Transactional(readOnly = true)
-	public List<ProductDTO> findAll() {
-		final List<Product> list = this.repository.findAll();
-		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
-	}
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(final PageRequest pageRequest) {
@@ -48,7 +45,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(final ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		this.copyDtoToEntity(dto, entity);
 		entity = this.repository.save(entity);
 		return new ProductDTO(entity);
 	}
@@ -57,7 +54,7 @@ public class ProductService {
 	public ProductDTO update(final Long id, final ProductDTO dto) {
 		try {
 			Product entity = this.repository.getOne(id);
-			//entity.setName(dto.getName());
+			this.copyDtoToEntity(dto, entity);
 			entity = this.repository.save(entity);
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException e) {
@@ -72,6 +69,21 @@ public class ProductService {
 			throw new ResourceNotFoundException("Id not found : " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(final ProductDTO dto, final Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = this.categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
 		}
 	}
 }
