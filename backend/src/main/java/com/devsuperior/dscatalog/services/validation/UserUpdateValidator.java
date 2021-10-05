83af958e102492f63b5b541aee77148bf5a8db48
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-10-05 15:06:53 -0400

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
@@ -2,11 +2,14 @@ package com.devsuperior.dscatalog.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.entities.User;
@@ -14,6 +17,9 @@ import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository repository;
@@ -25,12 +31,17 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 	@Override
 	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
 		
+		@SuppressWarnings("unchecked")
+		final var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		final long userId = Long.parseLong(uriVars.get("id"));
+		
+		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		User user = repository.findByEmail(dto.getEmail());
 		
 		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
-		if (user != null) {
+		if (user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "Ce courriel existe déjà"));
 		}
 		
