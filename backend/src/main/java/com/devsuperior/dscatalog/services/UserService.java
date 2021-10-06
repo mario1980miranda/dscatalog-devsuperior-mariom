8eaee7f70883cfa1571e121a8ad7275563c877d8
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-10-06 14:28:20 -0400

    **** Implementing Oauth2 authorization server ****

--- backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +30,10 @@ import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	@Autowired
@@ -77,8 +84,24 @@ public class UserService {
 			throw new DatabaseException("Integrity violation");
 		}
 	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		final User user = repository.findByEmail(username);
+		
+		if (user == null) {
+			logger.error("User not found : " + username);
+			throw new UsernameNotFoundException("Email non trouvé");
+		}
+		
+		logger.info("User found : " + username);
+		
+		return user;
+	}
 	
 	private void copyDtoToEntity(UserDTO dto, User entity) {
+		
 		entity.setFirstName(dto.getFirstName());
 		entity.setLastName(dto.getLastName());
 		entity.setEmail(dto.getEmail());
@@ -90,4 +113,5 @@ public class UserService {
 			entity.getRoles().add(role);
 		}
 	}
+
 }
