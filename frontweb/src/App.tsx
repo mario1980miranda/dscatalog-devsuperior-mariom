ec517dac9fe3134b0f20902771af75e663bbdff8
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-11-10 11:14:15 -0500

    components and layout x-small

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,10 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
-import Navbar from "components/Navbar";
+import Home from "pages/Home";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <h1>Hello DS Catalog</h1>
-    </>
+    <Home/>
   );
 }
 
