3f321f0ec86462bc986f2ad7012680d77e8a4fbf
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-11-11 10:37:01 -0500

    finishing home style

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as MainImage } from "assets/images/main-image.svg";
 import ButtonIcon from "components/ButtonIcon";
 import Navbar from "components/Navbar";
 
-import './styles.css';
+import "./styles.css";
 
 const Home = () => {
   return (
@@ -11,9 +11,14 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça o melhor catalogo de produtos</h1>
-            <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
-            <ButtonIcon/>
+            <div>
+              <h1>Conheça o melhor catalogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
             <MainImage />
