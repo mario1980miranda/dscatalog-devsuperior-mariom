17d04849ac9d42b4617a3186b2ff16e4c49cc7ca
Mario Miranda <mario_chan15@hotmail.com>
2022-07-03 22:46:57 -0400

    Iniciando catalogo de produtos

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,10 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
-import Home from "pages/Home";
+import Catalog from "pages/Catalog";
 
 const App = () => {
   return (
-    <Home />
+    <Catalog />
   );
 }
 
---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from "components/Navbar";
+import "./styles.css";
+
+const Catalog = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-4">
+        <h1>Catálogo de produtos</h1>
+      </div>
+    </>
+  );
+};
+
+export default Catalog;
