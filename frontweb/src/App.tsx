ef60665b8c7b28300f07d2c1f5918ee279c459a2
Mario Miranda <mario_chan15@hotmail.com>
2022-07-04 09:27:00 -0400

    Page Admin

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,11 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
 import Catalog from "pages/Catalog";
+import Admin from "pages/Admin";
 
 const App = () => {
   return (
-    <Catalog />
+    <Admin />
   );
 }
 
