f00728824b2479d45a9fb7ddb8d49b1f13d05464
Mario Miranda <mario_chan15@hotmail.com>
2022-07-05 14:22:40 -0400

    ProductDetails - estrutura Html

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,30 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
 const ProductDetails = () => {
     return(
-        <h1>Product Details</h1>
+        <div className="product-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>Voltar</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg" alt="Nome do produto" />
+                            <ProductPrice price={234.56} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h1>Descrição do produto</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, consequatur.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
