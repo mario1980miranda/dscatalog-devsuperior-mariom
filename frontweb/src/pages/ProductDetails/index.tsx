00a91a498188dcc3a1f9f759d5eb881a79455ae3
Mario Miranda <mario_chan15@hotmail.com>
2022-07-05 22:24:43 -0400

    Estrutura layout ProductDetails

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,12 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 
+import './styles.css';
+
 const ProductDetails = () => {
     return(
         <div className="product-details-container">
-            <div className="product-details-card">
+            <div className="base-card product-details-card">
                 <div className="goback-container">
                     <ArrowIcon />
                     <h2>Voltar</h2>
@@ -13,12 +15,15 @@ const ProductDetails = () => {
                     <div className="col-xl-6">
                         <div className="img-container">
                             <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg" alt="Nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
                             <ProductPrice price={234.56} />
                         </div>
                     </div>
                     <div className="col-xl-6">
                         <div className="description-container">
-                            <h1>Descrição do produto</h1>
+                            <h2>Descrição do produto</h2>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, consequatur.</p>
                         </div>
                     </div>
