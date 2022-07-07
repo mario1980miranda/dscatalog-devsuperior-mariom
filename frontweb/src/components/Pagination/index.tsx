81a127400bbba35020ed057e4984d865124312c9
Mario Miranda <mario_chan15@hotmail.com>
2022-07-07 09:36:59 -0400

    Component - Pagination

---
+++ frontweb/src/components/Pagination/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+
+import './styles.css';
+
+const Pagination = () => {
+    return(
+        <div className="pagination-container">
+            <ArrowIcon />
+            <div className="pagination-item">1</div>
+            <div className="pagination-item">2</div>
+            <div className="pagination-item">3</div>
+            <div className="pagination-item">...</div>
+            <div className="pagination-item">10</div>
+            <ArrowIcon />
+        </div>
+    );
+}
+
+export default Pagination;
