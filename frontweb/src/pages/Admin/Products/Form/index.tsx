9a096cb5a8e73defbd9e981aa019d90f565798a8
Mario Miranda <mario_chan15@hotmail.com>
2022-07-22 13:02:16 -0400

    ProductCrudForm - Style I

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,17 +6,27 @@ const Form = () => {
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">Dados do produto</h1>
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-              <input type="text" className="form-control base-input"/>
-              <input type="text" className="form-control base-input"/>
-              <input type="text" className="form-control base-input"/>
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="product-crud-input margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="product-crud-input margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="product-crud-input">
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea cols={30} rows={10} className="form-control base-input"></textarea>
+              <textarea
+                cols={30}
+                rows={10}
+                className="form-control base-input"
+              ></textarea>
             </div>
           </div>
-          <div>
+          <div className="product-crud-buttons-container">
             <button className="btn btn-outline-danger">CANCELAR</button>
             <button className="btn btn-primary">SALVAR</button>
           </div>
