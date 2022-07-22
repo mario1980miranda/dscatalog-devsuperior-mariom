0ee46f9d4a96fff5dec2f02445a356625ace25e7
Mario Miranda <mario_chan15@hotmail.com>
2022-07-22 11:26:07 -0400

    Product crud card - mobile layout

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -25,6 +25,14 @@ const ProductCrudCard = ({ product }: Props) => {
           ))}
         </div>
       </div>
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-buttom product-crud-card-buttom-first">
+          EXCLUIR
+        </button>
+        <button className="btn btn-outline-secondary product-crud-card-buttom">
+          EDITAR
+        </button>
+      </div>
     </div>
   );
 };
