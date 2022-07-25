341099276171a767b27c2818a0ee929335932081
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 09:55:14 -0400

    Add component React-Paginate

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,9 +1,17 @@
 import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import ReactPaginate from 'react-paginate';
 
 import './styles.css';
 
 const Pagination = () => {
     return(
+        <>
+        <ReactPaginate 
+            pageCount={10}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={1}
+        />
+
         <div className="pagination-container">
             <ArrowIcon className="arrow-previous arrow-inative" />
             <div className="pagination-item active">1</div>
@@ -13,6 +21,7 @@ const Pagination = () => {
             <div className="pagination-item">10</div>
             <ArrowIcon className="arrow-next arrow-active" />
         </div>
+        </>
     );
 }
 
