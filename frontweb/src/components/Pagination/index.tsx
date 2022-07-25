65a9c91aa53e520bf9763a6bb33cd8ca6073bb1b
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 10:52:57 -0400

    Catalog - Pagination

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -23,8 +23,8 @@ const Pagination = ({pageCount, range, onChange}:Props) => {
       activeLinkClassName="pagination-link-active"
       disabledClassName="arrow-inactive"
 
-      previousLabel={<ArrowIcon />}
-      nextLabel={<ArrowIcon />}
+      previousLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
+      nextLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
 
       onPageChange={(items) => onChange ? onChange(items.selected) : {}}
     />
