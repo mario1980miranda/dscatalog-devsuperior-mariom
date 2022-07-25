2bbd9959bfc58d5eec62f333094167a868b5006f
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 12:17:06 -0400

    ProductFilter - HTML

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -1,9 +1,23 @@
 import './styles.css';
 
 const ProductFilter = () => {
-    return(
-        <div className="base-card product-filter-container">Search bar</div>
-    );
+  return (
+    <div className="base-card product-filter-container">
+      <form action="" className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input className="form-control" />
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select>
+              <option value="">Livros</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">LIMPAR</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ProductFilter;
