5e1a780232149e10cb36980e6284e7b44c01c9a6
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 10:10:12 -0400

    React-Paginate - Style

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,28 +1,25 @@
-import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ReactPaginate from 'react-paginate';
 
 import './styles.css';
 
 const Pagination = () => {
-    return(
-        <>
-        <ReactPaginate 
-            pageCount={10}
-            pageRangeDisplayed={3}
-            marginPagesDisplayed={1}
-        />
-
-        <div className="pagination-container">
-            <ArrowIcon className="arrow-previous arrow-inative" />
-            <div className="pagination-item active">1</div>
-            <div className="pagination-item">2</div>
-            <div className="pagination-item">3</div>
-            <div className="pagination-item">...</div>
-            <div className="pagination-item">10</div>
-            <ArrowIcon className="arrow-next arrow-active" />
-        </div>
-        </>
-    );
-}
+  return (
+    <ReactPaginate
+      pageCount={10}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      containerClassName="pagination-container"
+      pageLinkClassName="pagination-item"
+      breakClassName="pagination-item"
+      previousLabel={<ArrowIcon />}
+      previousClassName="arrow-previous"
+      nextLabel={<ArrowIcon />}
+      nextClassName="arrow-next"
+      activeLinkClassName="pagination-link-active"
+      disabledClassName="arrow-inactive"
+    />
+  );
+};
 
 export default Pagination;
