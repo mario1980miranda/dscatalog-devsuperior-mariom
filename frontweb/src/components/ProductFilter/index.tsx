a5d4924f5f6955f06e781cbc35a9b63a5efaeaa0
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 12:30:51 -0400

    ProductFilter - input name laytout

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -1,3 +1,4 @@
+import {ReactComponent as SearchIcon} from 'assets/images/search-icon.svg';
 import './styles.css';
 
 const ProductFilter = () => {
@@ -5,7 +6,8 @@ const ProductFilter = () => {
     <div className="base-card product-filter-container">
       <form action="" className="product-filter-form">
         <div className="product-filter-name-container">
-          <input className="form-control" />
+          <input className="form-control" placeholder="Nome do produto" />
+          <SearchIcon />
         </div>
         <div className="product-filter-bottom-container">
           <div className="product-filter-category-container">
