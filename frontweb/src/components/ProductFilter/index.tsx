56aa64d790ced84bea53f82c9699606041724e8b
Mario Miranda <mario_chan15@hotmail.com>
2022-07-25 14:32:41 -0400

    ProductFilter - internal behavor

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -9,16 +9,33 @@ import './styles.css';
 
 type ProductFilterData = {
   name: string;
-  category: Category;
+  category: Category | null;
 };
 
 const ProductFilter = () => {
   const [selectCategories, setSelectCategories] = useState<Category[]>();
 
-  const { register, handleSubmit, control } = useForm<ProductFilterData>();
+  const { register, handleSubmit, setValue, getValues, control } =
+    useForm<ProductFilterData>();
 
   const onsubmit = (formData: ProductFilterData) => {
     console.log('ENVIOU', formData);
+  };
+
+  const handleFormClear = () => {
+    setValue('name', '');
+    setValue('category', null);
+  };
+
+  const handleChangeCategory = (value: Category) => {
+    setValue('category', value);
+
+    const obj: ProductFilterData = {
+      name: getValues('name'),
+      category: getValues('category'),
+    };
+
+    console.log(obj);
   };
 
   useEffect(() => {
@@ -54,13 +71,17 @@ const ProductFilter = () => {
                   classNamePrefix="product-filter-select"
                   isClearable
                   placeholder="Categoria"
+                  onChange={(value) => handleChangeCategory(value as Category)}
                   getOptionLabel={(category: Category) => category.name}
                   getOptionValue={(category: Category) => String(category.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary btn-product-filter-clear">
+          <button
+            onClick={handleFormClear}
+            className="btn btn-outline-secondary btn-product-filter-clear"
+          >
             LIMPAR <span className="btn-product-filter-word">FILTRO</span>
           </button>
         </div>
