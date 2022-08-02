db4e5f7c089ab7e5fd1fd409932986bc3a8622c7
mario1980miranda <mario_chan15@hotmail.com>
2022-08-02 09:45:32 -0400

    Pagination click tests

--- frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -47,4 +47,37 @@ describe('Pagination tests', () => {
 
     expect(onChange).toHaveBeenCalledWith(1);
   });
+
+  test('previous arrow should call onChange', async () => {
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn();
+    const forcePage = 1;
+
+    render(
+      <Pagination pageCount={pageCount} range={range} onChange={onChange} forcePage={forcePage} />
+    );
+
+    const arrowPrevious = screen.getByTestId('arrow-previous');
+
+    await userEvent.click(arrowPrevious);
+
+    expect(onChange).toHaveBeenCalledWith(0);
+  });
+
+  test('page link should call onChange', async () => {
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn();
+
+    render(
+      <Pagination pageCount={pageCount} range={range} onChange={onChange} />
+    );
+
+    const page2 = screen.getByText('2');
+
+    await userEvent.click(page2);
+
+    expect(onChange).toHaveBeenCalledWith(1);
+  });
 });
